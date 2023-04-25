aa099a0a31e7ac40ea95d2ce1d3ba4621f79a796
Andres Soria <andres.zov@gmail.com>
2023-04-25 16:32:25 -0600

    Set up basic markup for calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
 function App() {
-  return <div className='App'>Hello, world!</div>;
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(<button key={i}>{i}</button>);
+    }
+
+    return digits;
+  };
+
+  return (
+    <div className='calculator'>
+      <div className='display'>
+        <span>(0)</span> 0
+      </div>
+      <div className='operators'>
+        <button>/</button>
+        <button>*</button>
+        <button>+</button>
+        <button>-</button>
+
+        <button>DEL</button>
+      </div>
+      <div className='digits'>
+        {createDigits()}
+        <button>0</button>
+        <button>.</button>
+        <button>=</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
